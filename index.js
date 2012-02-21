0586a299d306adcb15cd912959da329e3c5b5514
Todd Wolfson <todd@twolfson.com>
2012-02-21 14:02:32 -0800

    Double heart complete

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
-// Attribution to http://mathworld.wolfram.com/HeartCurve.html
-M=Math;
-cos=M.cos;
-function heartX(t) {
-  return 16 * M.pow(M.sin(t), 3);
-}
-
-function heartY(t) {
-  return 13 * cos(t) - 5 * cos(2*t) - 2 * cos(3*t) - cos(4*t);
-}
-
-a.beginPath();
-for (i = 0, cap = 1e3, t = 0; i < cap; i++) {
-  t = i ? M.PI * 2 * i/cap : 0;
-  a.lineTo(heartX(t) + 50, -heartY(t) + 50);
-  a.stroke();
-}
-a.fill();
-a.closePath();
+// s='px sans-serif';
+a.font = '50px sans-serif';
+a.fillStyle = 'red';
+a.fillText('♥', 100, 100);
+a.font = '24px sans-serif';
+a.fillStyle = 'white';
+a.fillText('♥', 108, 93);
